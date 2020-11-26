11654dfbae6f14359eb99631d71340b35d161b25
llliuer <741682423@qq.com>
2020-11-26 20:47:28 +0800

    ..

--- leetcode-python/前序遍历二叉树.py
+++ leetcode-python/前序遍历二叉树.py
@@ -27,3 +27,17 @@ class Solution:
 
         dfs(root)
         return res
+
+#官方题解：
+# class Solution:
+#     def preorderTraversal(self, root: TreeNode) -> List[int]:
+#         def preorder(root: TreeNode):
+#             if not root:
+#                 return
+#             res.append(root.val)
+#             preorder(root.left)
+#             preorder(root.right)
+
+#         res = list()
+#         preorder(root)
+#         return res
