7ff525c21942b2cbfe7a52399cd32d2d287d0b56
llliuer <741682423@qq.com>
2020-11-26 20:45:47 +0800

    ..

--- leetcode-python/上升下降字符串.py
+++ leetcode-python/上升下降字符串.py
@@ -1,6 +1,8 @@
 
 
 #leetcode 1370. 上升下降字符串
+#https://leetcode-cn.com/problems/increasing-decreasing-string/
+#
 #
 #
 
@@ -34,5 +36,28 @@ class Solution:
                     bucket[index] -= 1
                 index -= 1
         return res
+
+
+class Solution:
+    def sortString(self, s: str) -> str:
+        num = [0] * 26
+        for ch in s:
+            num[ord(ch) - ord('a')] += 1
+
+        ret = list()
+        while len(ret) < len(s):
+            for i in range(26):
+                if num[i]:
+                    ret.append(chr(i + ord('a')))
+                    num[i] -= 1
+            for i in range(25, -1, -1):
+                if num[i]:
+                    ret.append(chr(i + ord('a')))
+                    num[i] -= 1
+
+        return "".join(ret)
+
+
+
 
 
---
+++ leetcode-python/前序遍历二叉树.py
@@ -0,0 +1,29 @@
+
+#144.二叉树的前序遍历
+#https://leetcode-cn.com/problems/binary-tree-preorder-traversal/submissions/
+#
+
+#递归，对一个节点，先输出根节点，再递归输出左节点右节点
+#结束返回条件是遍历到空节点
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+
+        def dfs(root):
+            if root == None:
+                return root
+            res.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+
+            return
+
+        dfs(root)
+        return res
